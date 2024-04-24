811cc9092b46a54fc924c512ca134ca4a44cc3a8
Alec Philavong <alecphil@live.com>
2024-04-24 14:05:11 -0500

    finished of 72

---
+++ rule_of_72.js
@@ -0,0 +1,8 @@
+let interest = 3.3
+let savingsAccount = 5076.32;
+let rule = 72;
+let compound = savingsAccount.toFixed(2) * 2;
+let  yearsDouble = 72 / interest;
+
+console.log("Your acount balance is currently " + " $ " +savingsAccount+ " and with a " + interest + " %insterest rate, your savings account will be worth $ " + compound + " in " + yearsDouble.toFixed(2) + " years ") 
+
