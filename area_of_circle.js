418f4d772c20cae616b86911ccb07dd2be118ee1
Alec Philavong <alecphil@live.com>
2024-04-24 11:58:49 -0500

    finished area of circle

---
+++ area_of_circle.js
@@ -0,0 +1,8 @@
+let radius = 15;
+let pi = Math.PI;
+let areaOfCircle = (radius,PI) => {
+
+    return pi * radius * radius;
+};
+
+console.log("The area of the circle is " + areaOfCircle(radius,pi));
