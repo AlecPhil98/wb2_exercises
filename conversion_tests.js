63be1fccbd5b74f64b82cc3b38cbaa6094eeaa09
Alec Philavong <alecphil@live.com>
2024-04-25 13:33:08 -0500

    finished conversion tests

---
+++ conversion_tests.js
@@ -0,0 +1,35 @@
+"use strict"
+
+let a = "101.1";
+let b = "55";
+let c = "402 Stevens";
+var d = "Number 5";
+
+//using parseInt for each vairable and dispalying the results 
+
+console.log("variable a with parseInt:" + parseInt(a));
+console.log("variable b with parseInt:" + parseInt(b));
+console.log("variable c with parseInt:" + parseInt(c));
+console.log("variable c with parseInt:" + parseInt(d));
+
+//using parseFloat for each variable and displaying the results 
+
+console.log("variable a with parseFloat" + parseFloat(a));
+console.log("variable b with parseFloat" + parseFloat(b));
+console.log("variable c with parseFloat" + parseFloat(c));
+console.log("variable d with parseFloat" + parseFloat(d));
+
+//using Number for eash variable and displying the results 
+console.log("variable a with number" + Number(a));
+console.log("variable b with number" + Number(b));
+console.log("variable c with number" + Number(c));
+console.log("variable d with number" + Number(d));
+
+//using unary (+) for each variabvvle and displaying the results 
+
+console.log("variable a with unary " + +(a));
+console.log("variable b with unary " + +(b));
+console.log("variable c with unary " + +(c));
+console.log("variable d with unary " + +(d));
+
+
---
+++ distance.js
@@ -0,0 +1,5 @@
+let pointA = x1 - x2;
+let pointB = y1 - y2;
+
+let distance = Math.sqrt(pointA*pointA + pointB*pointB);
+
